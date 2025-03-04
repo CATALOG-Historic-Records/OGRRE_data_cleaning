aefb1a935e30129ca279378c30655edae03d5b90
Jacob-Shay <148918220+Jacob-Shay@users.noreply.github.com>
2025-03-04 14:53:21 -0800

    Excel_to_Json function for multiple schema spreadsheets
    CALGEM processor trained for Charu's team

--- processor_schemas/excel_to_json.py
+++ processor_schemas/excel_to_json.py
@@ -24,46 +24,47 @@ def write_json(Dataframe, Filepath, key_replacements=None):
     with open(Filepath, "w", encoding="utf-8") as json_file:
         json.dump(json_dict, json_file, indent=4, default=str)
 
-Test_Sheet_Names = False
+def Excel_to_Json(excel_file_path = "ISGS Well Completion Schema.xlsx", Test_Sheet_Names = False):
+    Organization = excel_file_path[:excel_file_path.find(' ')].lower()
+    # Load the Excel file
+    excel_file = pd.ExcelFile(excel_file_path)
+    sheet_names = excel_file.sheet_names
+    print(sheet_names)
+    
+    Trained_Models_df = pd.read_excel(excel_file_path, sheet_name="Trained Models")
+    Extractors = Trained_Models_df[(Trained_Models_df["Processor Type"] == "Extractor")*(Trained_Models_df["Primary Model in Processor"] == "primary")]
 
-# Load the Excel file
-excel_file_path = "ISGS Well Completion Schema.xlsx"
+    if Test_Sheet_Names:
+        for processor in Extractors["Processor Name"]:
+            print(np.any(processor in sheet_names))
 
-excel_file = pd.ExcelFile(excel_file_path)
+    key_replacements = {
+        "Google Processor Name": "google_processor_name",
+        "OGRRE Document Type": "document_type",
+        "Page Order Sort": "page_order_sort",
+        "Name": "name",
+        "Google Data type": "google_data_type",
+        "Occurrence": "occurrence",
+        "Grouping": "grouping",
+        "Database Data Type": "database_data_type",
+        "Cleaning Function": "cleaning_function",
+        "Accepted Range": "accepted_range",
+        "Field Specific Notes": "field_specific_notes",
+        "Model Enabled": "model_enabled"
+    }
 
-Trained_Models_df = pd.read_excel(excel_file_path, sheet_name="Trained Models")
-
-sheet_names = excel_file.sheet_names
-print(sheet_names)
-
-Extractors = Trained_Models_df[(Trained_Models_df["Processor Type"] == "Extractor")*(Trained_Models_df["Primary Model in Processor"] == "primary")]
-# print(f"extractors : {Extractors}")
-
-if Test_Sheet_Names:
+    
+    extractors_dir = f"{Organization}_extractors"
+    if not os.path.exists(extractors_dir):
+        os.makedirs(extractors_dir)
+    
+    write_json(Dataframe = Extractors.reset_index()[Extractors.columns], Filepath = f"{Organization}_extractors/00Extractor Processors.json", key_replacements=key_replacements)
+    
     for processor in Extractors["Processor Name"]:
-        print(np.any(processor in sheet_names))
-
-key_replacements = {
-    "Google Processor Name": "google_processor_name",
-    "OGRRE Document Type": "document_type",
-    "Page Order Sort": "page_order_sort",
-    "Name": "name",
-    "Google Data type": "google_data_type",
-    "Occurrence": "occurrence",
-    "Grouping": "grouping",
-    "Database Data Type": "database_data_type",
-    "Cleaning Function": "cleaning_function",
-    "Accepted Range": "accepted_range",
-    "Field Specific Notes": "field_specific_notes",
-    "Model Enabled": "model_enabled"
-}
-
-write_json(Dataframe = Extractors.reset_index()[Extractors.columns], Filepath = "Extractor Processors.json", key_replacements=key_replacements)
-
-extractors_dir = "isgs_extractors"
-if not os.path.exists(extractors_dir):
-    os.makedirs(extractors_dir)
-
-for processor in Extractors["Processor Name"]:
-    processor_df = pd.read_excel(excel_file_path, sheet_name=processor)
-    write_json(Dataframe =  processor_df, Filepath = f"isgs_extractors/{processor}.json", key_replacements=key_replacements)
+        processor_df = pd.read_excel(excel_file_path, sheet_name=processor)
+        write_json(Dataframe =  processor_df, Filepath = f"{Organization}_extractors/{processor}.json", key_replacements=key_replacements)
+        
+        
+if __name__ == '__main__':
+    Excel_to_Json(excel_file_path = "ISGS Well Completion Schema.xlsx", Test_Sheet_Names = False)
+    Excel_to_Json(excel_file_path = "CALGEM Well Summary Schema.xlsx", Test_Sheet_Names = False)
