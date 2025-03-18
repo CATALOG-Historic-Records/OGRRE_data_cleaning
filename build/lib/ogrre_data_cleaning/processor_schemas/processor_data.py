99ad6dfe83e6b73986054b015f33bdb9789c7f2a
MichaelPesce <michaelcpesce@gmail.com>
2025-03-18 15:30:13 -0500

    remove build and reinstall

--- build/lib/ogrre_data_cleaning/processor_schemas/processor_data.py
+++ build/lib/ogrre_data_cleaning/processor_schemas/processor_data.py
@@ -1,11 +1,15 @@
 import importlib.resources
 import json
 
-def get_processor_list(collaborator: str = "isgs"):
-    with importlib.resources.open_text(__package__+f".{collaborator}_extractors", "00Extractor Processors.json") as f:
-        return json.load(f)
+def get_processor_list(collaborator: str):
+    try:
+        with importlib.resources.open_text(__package__+f".{collaborator}_extractors", "00Extractor Processors.json") as f:
+            return json.load(f)
+    except Exception as e:
+        print(f"unable to find processor list for collaborator: {collaborator}")
+        return None
     
-def get_processor_by_id(collaborator: str = "isgs", processor_id: str = None):
+def get_processor_by_id(collaborator: str, processor_id: str):
     if not processor_id:
         return None
 
@@ -17,14 +21,16 @@ def get_processor_by_id(collaborator: str = "isgs", processor_id: str = None):
             processor_data = extractor
             break
     if not processor_data:
-        print(f"processor data is none")
+        print(f"unable to find processor data for {collaborator} id: {processor_id}")
         return None
     
     processor_name = processor_data.get("Processor Name")
-    print(f"found processor with name {processor_name}")
 
-    with importlib.resources.open_text(__package__+f".{collaborator}_extractors", f"{processor_name}.json") as f:
-        processor_data["attributes"] = json.load(f)
+    try:
+        with importlib.resources.open_text(__package__+f".{collaborator}_extractors", f"{processor_name}.json") as f:
+            processor_data["attributes"] = json.load(f)
+    except Exception as e:
+        print(f"unable to find attributes for {collaborator} id: {processor_id}")
 
     return processor_data
 
@@ -32,8 +38,7 @@ def get_processor_by_name(collaborator: str = "isgs", processor_name: str = None
     if not processor_name:
         return None
 
-    with importlib.resources.open_text(__package__+f".{collaborator}_extractors", "00Extractor Processors.json") as f:
-        extractor_data = json.load(f)
+    extractor_data = get_processor_list()
 
     processor_data = None
     for extractor in extractor_data:
@@ -41,13 +46,24 @@ def get_processor_by_name(collaborator: str = "isgs", processor_name: str = None
             processor_data = extractor
             break
     if not processor_data:
+        print(f"unable to find processor data for {collaborator} named: {processor_name}")
         return None
     
-    with importlib.resources.open_text(__package__+f".{collaborator}_extractors", f"{processor_name}.json") as f:
-        processor_data["attributes"] = json.load(f)
+    try:
+        with importlib.resources.open_text(__package__+f".{collaborator}_extractors", f"{processor_name}.json") as f:
+            processor_data["attributes"] = json.load(f)
+    except Exception as e:
+        print(f"unable to find attributes for {collaborator} named: {processor_name}")
 
     return processor_data
 
-def get_processor_image(collaborator: str = "isgs", processor_name: str = None):
-    with importlib.resources.open_text(__package__+f".{collaborator}_extractors.images", f"{processor_name}.png") as f:
-        return f.read()
+def get_processor_image(collaborator: str, processor_name: str):
+    if processor_name is None:
+        return None
+    try:
+        with importlib.resources.open_text(__package__+f".{collaborator}_extractors.images", f"{processor_name}.png") as f:
+            return f.read()
+    except Exception as e:
+        print(f"unable to find processor images for {collaborator} : {processor_name}")
+        return None
+    
